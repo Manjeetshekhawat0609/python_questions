638931971077028bc75a2b55d8c63b32ae48a97f
Manjeetshekhawat0609 <manjeetshekhawat0609@gmail.com>
2026-07-28 11:19:04 +0530

    Initial commit

---
+++ greet.py
@@ -0,0 +1,48 @@
+# Global variable
+# x = 100
+# def display():
+#     print(x)
+
+# display()
+
+
+# local variable
+# def display():
+#     y = 20
+#     print(y)
+
+# display()
+
+
+
+# def greet():
+#     msg = "hello from inside the function!"
+#     print(msg)
+# greet()
+
+# def greet():
+#     msg = "hello!"
+#     print("inside function:", msg)
+# greet()
+# print("outside function:", msg)
+
+
+
+# msg ="python is awesome!"
+# def greet():
+#     msg = "hello!"
+#     print("inside function:", msg)
+# greet()
+# print("outside function:", msg)
+
+
+
+def fun():
+    s = "Me too."
+    print(s)
+
+s = "I love Geeksforgeeks"
+fun()
+print(s)
+
+
---
+++ primenumber.py
@@ -0,0 +1,51 @@
+# num = int(input("enter a number: "))
+
+# if num <= 1:
+#     print(num, "is not a prime no.")
+# else :
+#     is_prime = True
+
+#     for i in range (2, num):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+#     if is_prime:
+#         print(num, "is a prime no.")
+#     else:
+#         print(num,"is not a prime no.")
+
+
+
+# num = int(input("enter a number: "))
+
+# if num <= 1:
+#     print(num, "is not a prime no.")
+# else :
+#     is_prime = True
+
+#     for i in range (2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+#     if is_prime:
+#         print(num, "is a prime no.")
+#     else:
+#         print(num,"is not a prime no.")               
+
+
+
+# n = int(input("Enter the value of N: "))
+# print("Prime numbers are:")
+
+# for num in range(2, n + 1):
+#     is_prime = True
+
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+
+#     if is_prime:
+#         print(num, end=" ")
+
+
