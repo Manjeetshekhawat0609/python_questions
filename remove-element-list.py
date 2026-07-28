55b37bb6d33e9b2d45bbf02b1a79ee0bfe5cd659
Manjeetshekhawat0609 <manjeetshekhawat0609@gmail.com>
2026-07-28 11:52:02 +0530

    Added remove element list program

---
+++ remove-element-list.py
@@ -0,0 +1,24 @@
+# marks = [90, 60, 80]
+
+# print(marks)
+# # Output: [90, 60, 80]
+
+# marks.remove(60)
+
+# print(marks)
+# # Output: [90, 80]
+
+
+
+
+
+marks = [90, 60, 80]
+
+print(marks)
+# Output: [90, 60, 80]
+
+marks.pop(1)
+
+
+print(marks)
+# Output: [90, 80]
