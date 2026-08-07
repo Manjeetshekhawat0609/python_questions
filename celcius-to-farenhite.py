6d66f24370126992cbaaf743d5af34028ee7ad84
Manjeetshekhawat0609 <manjeetshekhawat0609@gmail.com>
2026-08-07 12:11:20 +0530

    add list operators

--- celcius-to-farenhite.py
+++ celcius-to-farenhite.py
@@ -3,6 +3,6 @@ celsius = (farenhite - 32)*5/9
 print("farenhite to celcius= ", celsius)
 
 
-# celsius = float(input("enter temprature in c: "))
-# fahrenheit = (celsius*9/5)+32
-# print("celcius to fahrenheit: ", fahrenheit)
+celsius = float(input("enter temprature in c: "))
+fahrenheit = (celsius*9/5)+32
+print("celcius to fahrenheit: ", fahrenheit)
