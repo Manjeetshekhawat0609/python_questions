c44c6271a32bc49ad7713c7924ebab4f44b85648
Manjeetshekhawat0609 <manjeetshekhawat0609@gmail.com>
2026-08-14 11:15:36 +0530

    add tuple

--- celcius-to-farenhite.py
+++ celcius-to-farenhite.py
@@ -1,7 +1,10 @@
-farenhite = float(input("Enter temperature in f: "))
-celsius = (farenhite - 32)*5/9
-print("farenhite to celcius= ", celsius)
+# farenhite to celcius
 
+fahrenheit = float(input("Enter temperature in f: "))
+celsius = (fahrenheit - 32)*5/9
+print("farenhite to celcius: ", celsius)
+
+# celcius to fahrenheit 
 
 celsius = float(input("enter temprature in c: "))
 fahrenheit = (celsius*9/5)+32
---
+++ tuple.py
@@ -0,0 +1,12 @@
+# thistuple = ("apple", "banana","cherry")
+# for x in thistuple:
+#     print(x)
+
+# tuple1 = ("a", "b", "c")
+# tuple2 = (1,2,3)
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+fruits = ("apple", "banana", "cherry")
+mytuple =  fruits * 2
+print(mytuple)
