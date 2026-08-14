cd8d3a23eee0857505b61c5ea347773851a36ce9
Manjeetshekhawat0609 <manjeetshekhawat0609@gmail.com>
2026-08-14 12:05:13 +0530

    add factorial

---
+++ factorial/factorial.py
@@ -0,0 +1,22 @@
+# '''f = 1
+# for n in range(20):
+#     print(n, "! =", f)
+#     n = n + 1
+#     f = f * n '''
+
+# # another method 
+
+# '''f = 1
+# n = 0
+# for n in range(20):
+#     print(n, "! =", f)
+#     n = n + 1
+#     f = f * n'''
+
+
+f = 1
+n = 1
+while f < 1000000000:
+    print(n, "! =",  f)
+    n = n +1
+    f = f * n
