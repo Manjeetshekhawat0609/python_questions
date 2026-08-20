f1951ac4fe220fbf2bee8b04de98ead507a8d965
Manjeetshekhawat0609 <manjeetshekhawat0609@gmail.com>
2026-08-20 09:46:02 +0530

    add set

--- set/thisset2.py
+++ set/thisset2.py
@@ -14,9 +14,28 @@ set2 = {1, 2, 3}
 set3 = set1 | set2 
 print(set3)
 
+#union multiple
 set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
 set3 = {"john", "elena"}
 set4 = {"apple", "banana", "cherry"}
 myset = set1.union(set2, set3, set4)
 print(myset)
+
+#method 2
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"john", "elena"}
+set4 = {"apple", "banana", "cherry"}
+myset = set1 | set2 | set3 | set4 
+print(myset)
+
+
+x = {"a", "b", "c"}
+y = (1, 2, 3)
+z = x.union(y)
+print(z)
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
